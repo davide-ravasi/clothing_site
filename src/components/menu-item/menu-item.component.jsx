d88152faf0d99ca9b27d59c2ed3c18eef3bfb11e
davide.ravasi <davide.ravasi@brandalley.com>
2019-10-14 10:10:49 +0200

    first commit -- last added feature google sign in

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        onClick={()=> history.push(`${match.url}${linkUrl}`)}
+        className={ `menu-item ${size}` }>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+)
+
+export default withRouter(MenuItem);
