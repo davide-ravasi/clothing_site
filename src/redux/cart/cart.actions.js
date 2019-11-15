3221e92dda69efc1f1650a760f7926e745498f6b
davide.ravasi <davide.ravasi@brandalley.com>
2019-11-15 17:38:02 +0100

    added addToCart reducer and button to list

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,8 @@
-const toggleCartHidden = () => ({
+export const toggleCartHidden = () => ({
     type: 'TOGGLE_CART_HIDDEN',
 })
 
-export default toggleCartHidden;
+export const addItem = (item) => ({
+    type: 'ADD_ITEM',
+    payload: item
+})
