e95f9691a97c8a736ea2517d8860495c3da1b1c8
davide.ravasi <davide.ravasi@brandalley.com>
2019-12-09 16:50:23 +0100

    added reducer cartitem component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,8 +1,15 @@
+import CartActionTypes from './cart.types';
+
 export const toggleCartHidden = () => ({
     type: 'TOGGLE_CART_HIDDEN',
 })
 
 export const addItem = (item) => ({
     type: 'ADD_ITEM',
+    payload: item
+})
+
+export const removeCartItem = (item) => ({
+    type: CartActionTypes.REMOVE_ITEM,
     payload: item
 })
